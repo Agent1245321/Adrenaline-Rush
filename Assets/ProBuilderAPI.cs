1db72dfaa85a83e5186c4a3eff09d08c38ac4fe7
Agent1245321 <112105034+Agent1245321@users.noreply.github.com>
2023-01-10 08:05:58 -0600

    GAHAHA

--- Assets/ProBuilderAPI.cs
+++ Assets/ProBuilderAPI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 
@@ -7,16 +8,16 @@ public class ProBuilderAPI : MonoBehaviour
 {
     public Material windowMatt;
     private ProBuilderMesh building;
-    public IReadOnlyCollection<int> indexes { get; }
+    public int[] tempQuad;
 
-    // public float xscale;
-    // public float yscale;
-    // public float zscale;
+     public float xscale;
+     public float yscale;
+     public float zscale;
 
     private void Start()
     {
 
-        /*  ProBuilderMesh quad = ProBuilderMesh.Create(
+          /*ProBuilderMesh quad = ProBuilderMesh.Create(
       new Vector3[] {
           new Vector3(xscale * 0f, zscale * 0f, zscale * 0f),
           new Vector3(xscale * 1f, yscale * 0f, zscale * 0f),
@@ -29,15 +30,30 @@ public class ProBuilderAPI : MonoBehaviour
       },
       new Face[] { new Face(new int[] { 1, 0, 3, 0, 2, 3 }), new Face(new int[] {0, 4, 2, 4, 6, 2 } ), new Face(new int[] {4, 5, 6, 5, 7, 6 } ), new Face(new int[] {5, 1, 7, 1, 3, 7 } ), new Face(new int[] {3, 2, 7, 2, 6, 7 } ), new Face(new int[] {0, 1, 4, 1, 5, 4 } )
 
-  });*/
+  }); */
         building = GetComponent<ProBuilderMesh>();
         foreach (Face window in building.faces)
         {
-            
-            Debug.Log(building.GetVertices());
-            
-            
+            if (window.IsQuad() == true)
+            {
+                window.indexes.CopyTo(tempQuad, 3);
+                Debug.Log(tempQuad[0]);
+                Debug.Log(tempQuad[1]);
+                Debug.Log(tempQuad[2]);
+                Debug.Log(tempQuad[3]);
+
+                new Face[] { new Face( new int[] {tempQuad})
+                
+
+                if (tempQuad[0] < tempQuad[2])
+                {
+                    building.TranslateVerticesInWorldSpace(tempQuad, new Vector3(1, 0, 0));
+                }
+            }
         }
+        
+        building.ToMesh();
+        building.Refresh();
         
      //   building.Refresh();
         
